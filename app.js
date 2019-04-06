3618e1e0fa14cf9276db5f52d225c1b81cef6ace
Vo Duc Dan <voducdand99@gmail.com>
2019-04-06 10:32:20 +0700

    add db connection

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-mongoose.connect('mongodb://localhost:27017/mongoose', {useNewUrlParser: true});
 
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/mongoose', {useNewUrlParser: true});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
