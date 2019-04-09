9acadc8996d0d86342b0992639cb25023e807f9e
Vo Duc Dan <voducdand99@gmail.com>
2019-04-09 11:12:03 +0700

    st

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // mongoose.connect(process.env.MONGOLAB_URI {useNewUrlParser: true});
-mongoose.connect(process.env.MONGOLAB_URI||'mongodb://localhost:27017/mongoose',{useNewUrlParser: true},function(error){console.log(error)});
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/mongoose', {
+  useNewUrlParser: true,
+  useMongoClient: true
+}, function (error) {
+  console.log(error)
+});
 mongoose.Promise = global.Promise;
 
 // view engine setup
@@ -21,7 +26,9 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
@@ -33,12 +40,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
